2c2a570cf88ab9a97f31cb88cd6c3cd56f263399
huanyu.yang <mfuture@qq.com>
2025-04-25 15:29:55 +0800

    feat: add README and image, update main.ts for NestExpressApplication

---
+++ README.zh.md
@@ -0,0 +1,5 @@
+## nestjs practice
+
+### platform
+
+![alt text](markdown/image.png)
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
 // Promises must be awaited, end with a call to .catch, end with a call to .then with a rejection handler
