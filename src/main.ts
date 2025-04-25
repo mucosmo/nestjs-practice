6ce4e8957be301821c57636d1c9e2f04530d4b46
huanyu.yang <mfuture@qq.com>
2025-04-25 15:29:27 +0800

    fix: ensure promises are awaited in bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+// Promises must be awaited, end with a call to .catch, end with a call to .then with a rejection handler
+// or be explicitly marked as ignored with the `void` operator.eslint@typescript-eslint/no-floating-promises
+void bootstrap();
