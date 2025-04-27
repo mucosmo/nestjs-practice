42276c98af057b020ea724d92eee1e895af8fe39
huanyu.yang <mfuture@qq.com>
2025-04-27 11:38:39 +0800

    feat: add LoggerMiddleware for request logging in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude('user/stream') // 排除 user/stream 路由
+      .forRoutes('*');
+  }
+}
