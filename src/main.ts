584efc3e62af3f031cc01c5fe813638405894e21
huanyu.yang <mfuture@qq.com>
2025-04-28 11:00:00 +0800

    feat: enhance logging in bootstrap function to include 'log' level messages

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NetworkUtil } from './utils/network.util';
+import { Logger } from '@nestjs/common'; // 导入 Logger
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    logger: ['error', 'warn'], // 只显示错误和警告日志
+    logger: ['error', 'warn', 'log'], // nestjs 框架显示日志
   });
   const port = process.env.PORT ?? 3300;
   await app.listen(port);
 
   const origin = NetworkUtil.getOrigin(+port);
-  console.log(`Server is listening at ${origin}`);
+  const logger = new Logger('Bootstrap');
+  logger.log(`Server is listening at ${origin}`);
 }
 // Promises must be awaited, end with a call to .catch, end with a call to .then with a rejection handler
 // or be explicitly marked as ignored with the `void` operator.eslint@typescript-eslint/no-floating-promises
