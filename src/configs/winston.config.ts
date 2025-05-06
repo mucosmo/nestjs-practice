954c41b42ace10fb5646bf440713464bac36dd31
huanyu.yang <mfuture@qq.com>
2025-05-06 09:54:43 +0800

    refactor: enhance Winston logging types and improve metadata handling

--- src/configs/winston.config.ts
+++ src/configs/winston.config.ts
@@ -5,6 +5,18 @@ import * as winston from 'winston';
 import * as packageJson from '../../package.json';
 import { ConfigEnum } from '../constants/config.constant';
 
+interface WinstonLogInfo extends winston.Logform.TransformableInfo {
+  timestamp: string;
+  level: string;
+  context: string;
+  ms: string;
+  label: string;
+  message: string; // 消息可以是任何类型
+  stack?: string[]; // 可选的堆栈信息
+  error?: Error; // 可选的错误对象
+  [key: string]: any; // 其他可能的元数据
+}
+
 /** 定义不同日志级别的颜色代码 */
 const colors = {
   error: '\x1b[31m', // 红色
@@ -32,27 +44,32 @@ const baseFormat = winston.format.combine(
     format: 'YY-MM-DD HH:mm:ss.SSS',
   }),
   winston.format.ms(),
-  // winston.format.errors({ stack: true }),
+  winston.format.errors({ stack: true }),
   winston.format.label({ label: packageJson.name, message: false }),
   winston.format.colorize({ level: false, message: false }),
   winston.format.uncolorize({ level: true, message: true }),
 );
 
-const printfFormatMessage = (info) => {
+const printfFormatMessage = (info: WinstonLogInfo) => {
   const { timestamp, level, context, ms, label, ...rest } = info;
   let { message } = info;
   if (level === 'error') {
     message = rest.stack?.[0] || message;
   }
-  if (message === 'undefined') message = '';
   // 如果有额外数据，添加为 JSON
   if (Object.keys(rest).length > 0 && !rest.splat) {
-    const metadata = { ...rest };
-    delete metadata.splat;
-    delete metadata.label;
-    delete metadata.message;
-    delete metadata.stack;
-    delete metadata.error;
+    // 创建要排除的属性集合
+    const excludeProps = ['splat', 'message', 'stack', 'error'];
+    // 创建一个新对象，仅包含不在排除列表中的属性
+    const metadata = Object.keys(rest)
+      .filter((key) => !excludeProps.includes(key))
+      .reduce(
+        (obj, key) => {
+          obj[key] = rest[key];
+          return obj;
+        },
+        {} as Record<string, any>,
+      );
     if (Object.keys(metadata).length > 0) {
       message += `${JSON.stringify(metadata)}`;
     }
@@ -63,7 +80,7 @@ const printfFormatMessage = (info) => {
 //TODO: 日志格式化会占用小于 1ms 的时间，
 //可以用 performance.now() 亚毫秒级别的时间来观察
 /**控制台日志打印格式 */
-const printfFormatConsole = (info) => {
+const printfFormatConsole = (info: WinstonLogInfo) => {
   const { timestamp, level, context, ms, label } = info;
   const levelUpper = level.toUpperCase().padStart(5, ' ');
   const message = printfFormatMessage(info);
@@ -78,7 +95,7 @@ const printfFormatConsole = (info) => {
 };
 
 /**文件日志打印格式 */
-const printfFormatFile = (info) => {
+const printfFormatFile = (info: WinstonLogInfo) => {
   const { timestamp, level, context, ms } = info;
   const message = printfFormatMessage(info);
   const levelUpper = level.toUpperCase().padStart(5, ' ');
@@ -90,7 +107,7 @@ export default registerAs(ConfigEnum.WINSTON, () => [
   new winston.transports.Console({
     format: winston.format.combine(
       baseFormat,
-      winston.format.printf((info) => printfFormatConsole(info)),
+      winston.format.printf((info: WinstonLogInfo) => printfFormatConsole(info)),
     ),
   }),
   new winston.transports.DailyRotateFile({
@@ -102,7 +119,7 @@ export default registerAs(ConfigEnum.WINSTON, () => [
     maxFiles: '31d',
     format: winston.format.combine(
       baseFormat,
-      winston.format.printf((info) => printfFormatFile(info)),
+      winston.format.printf((info: WinstonLogInfo) => printfFormatFile(info)),
     ),
   }),
   new winston.transports.DailyRotateFile({
@@ -114,7 +131,7 @@ export default registerAs(ConfigEnum.WINSTON, () => [
     maxFiles: '90d',
     format: winston.format.combine(
       baseFormat,
-      winston.format.printf((info) => printfFormatFile(info)),
+      winston.format.printf((info: WinstonLogInfo) => printfFormatFile(info)),
     ),
     level: 'error',
   }),
