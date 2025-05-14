e1106b62aca7baf5adf77706d14f15ae96c72426
huanyu.yang <mfuture@qq.com>
2025-05-14 18:15:27 +0800

    feat: enhance rate limit configuration with type definitions for better clarity

--- src/configs/ratelimit.config.ts
+++ src/configs/ratelimit.config.ts
@@ -1,4 +1,6 @@
+import { ExecutionContext } from '@nestjs/common';
 import { registerAs } from '@nestjs/config';
+import { Request } from 'express';
 
 import { ConfigEnum } from '../constants/config.constant';
 
@@ -9,9 +11,12 @@ export default registerAs(ConfigEnum.RATELIMIT, () => {
       limit: 10,
     },
   ];
-  const generateKey = (context) => {
-    const request = context.switchToHttp().getRequest();
-    const ip = request.ip || request.headers['x-forwarded-for'] || 'unknown';
+  const generateKey = (context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest<Request>();
+    let ip = request.ip || request.headers['x-forwarded-for'] || 'unknown';
+    if (Array.isArray(ip)) {
+      ip = ip[0];
+    }
     const path = request.route?.path || request.url;
     const method = request.method;
     return `${ip}-${path}-${method}`;
